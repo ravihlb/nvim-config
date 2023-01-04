2e6ea8b7197a4b94d0cfd744b15e3ff31a56463c
ravihlb <ravi@deliverydireto.com.br>
2023-01-04 19:07:57 -0300

    add treesitter-context to packer script

--- lua/profile/packer.lua
+++ lua/profile/packer.lua
@@ -22,6 +22,7 @@ return require('packer').startup(function(use)
     })
 
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use 'nvim-treesitter/nvim-treesitter-context'
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
 
