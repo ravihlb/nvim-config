6a4cc21f3297da8579a551fff4b3f070cd938fd1
ravihlb <ravi@deliverydireto.com.br>
2023-04-18 08:55:05 -0300

    feat(remaps): add visual line traversal and delete line

--- lua/profile/remap.lua
+++ lua/profile/remap.lua
@@ -13,7 +13,14 @@ vim.keymap.set("n", "-", ":vert resize -5<CR>", { silent = true })
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 
+vim.keymap.set("n", "j", "gj")
+vim.keymap.set("n", "k", "gk")
+
+vim.keymap.set("n", "0", "g0")
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "<F3>", ':d<CR>')
