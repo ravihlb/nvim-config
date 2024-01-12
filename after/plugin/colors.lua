50cf10c1ba012edfdb0d56570ae33562db44e359
ravihlb <ravi@deliverydireto.com.br>
2024-01-12 12:29:39 -0300

    refactor(colors): make SetupSonokai fn more generic

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,67 +1,32 @@
-function SetupSonokaiAtlantis()
-    vim.g.sonokai_style = 'atlantis'
-    vim.g.sonokai_better_performance = 1
-    vim.g.sonokai_transparent_background = 1
+function SetupSonokai(name)
+	vim.g.sonokai_style = name
+	vim.g.sonokai_better_performance = 1
+	vim.g.sonokai_transparent_background = 1
 end
 
-function SetupRosePine()
-    require('rose-pine').setup({
-	--- @usage 'main' | 'moon'
-	dark_variant = 'main',
-	bold_vert_split = false,
-	dim_nc_background = false,
-	disable_background = false,
-	disable_float_background = false,
-	disable_italics = false,
-
-	--- @usage string hex value or named color from rosepinetheme.com/palette
-	groups = {
-		background = 'base',
-		panel = 'surface',
-		border = 'highlight_med',
-		comment = 'muted',
-		link = 'iris',
-		punctuation = 'subtle',
-
-		error = 'love',
-		hint = 'iris',
-		info = 'foam',
-		warn = 'gold',
-
-		headings = {
-			h1 = 'iris',
-			h2 = 'foam',
-			h3 = 'rose',
-			h4 = 'gold',
-			h5 = 'pine',
-			h6 = 'foam',
-		}
-		-- or set all headings at once
-		-- headings = 'subtle'
-	},
-
-	-- Change specific vim highlight groups
-	highlight_groups = {
-		ColorColumn = { bg = 'rose' }
-	}
-})
-
--- set colorscheme after options
-vim.cmd('colorscheme rose-pine')
+function ToggleBackgroundTheme()
+	if vim.opt.background == "light" then
+		vim.opt.background = "dark"
+	else
+		if vim.opt.background == "dark" then
+			vim.opt.background = "light"
+		end
+	end
 end
 
-function ColorMyPencils(color)
-    if color ~= "rose-pine"
-    then
-        color = color or "sonokai"
-        SetupSonokaiAtlantis()
-    end
-    vim.cmd.colorscheme(color)
-    vim.opt.background = 'light'
+function ColorMyPencils(color, theme)
+	if theme == nil then
+		theme = "shusia"
+	end
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = 'none'})
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = 'none'})
+	if color ~= "rose-pine" then
+		color = color or "sonokai"
+		SetupSonokai(theme)
+	end
+	vim.cmd.colorscheme(color)
 
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
