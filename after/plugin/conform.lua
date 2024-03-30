aedfe39fdbc2e1c87dcdc4153530cb94bee974c5
ravihlb <ravihlb@protonmail.com>
2024-03-30 12:00:21 -0300

    feat(conform): add `FormatOnSaveToggle` user command

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -19,4 +19,23 @@ conform.setup({
 		python = { "isort", "black" },
 		bash = { "shfmt" },
 	},
+	format_on_save = function(bufnr)
+		-- Disable with a global or buffer-local variable
+		if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+			return
+		end
+		return { timeout_ms = 500, lsp_fallback = true }
+	end,
+})
+
+vim.api.nvim_create_user_command("FormatOnSaveToggle", function(args)
+	if args.bang then
+		-- FormatOnSaveToggle! will toggle just for current buffer
+		vim.b.disable_autoformat = not vim.b.disable_autoformat
+	end
+
+	vim.g.disable_autoformat = not vim.g.disable_autoformat
+end, {
+	desc = "Toggle autoformat on save.",
+	bang = true,
 })
