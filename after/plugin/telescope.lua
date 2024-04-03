829ac833cd8072646b6ad2d99808186d62b0a51b
ravihlb <ravihlb@protonmail.com>
2024-04-03 10:11:32 -0300

    feat(telescope): add project word search and use `vim.fn.input` for grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,14 +1,18 @@
 local keymap = vim.keymap.set
 local builtin = require("telescope.builtin")
-local git_worktree = require("telescope").extensions.git_worktree
 
 require("telescope").load_extension("git_worktree")
 
 keymap("n", "<C-p>", builtin.git_files, {})
 keymap("n", "<C-f>", builtin.find_files, {})
 keymap("n", "<C-h>", builtin.help_tags, {})
-keymap("n", "<leader>ps", builtin.live_grep, {})
 keymap("n", "<C-b>", builtin.buffers, {})
 
-keymap("n", "<leader>wl", git_worktree.git_worktrees, {})
-keymap("n", "<leader>wc", git_worktree.create_git_worktree, {})
+keymap("n", "<leader>pws", function()
+	local word = vim.fn.expand("<cWORD>")
+	builtin.grep_string({ search = word })
+end)
+
+keymap("n", "<leader>ps", function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
