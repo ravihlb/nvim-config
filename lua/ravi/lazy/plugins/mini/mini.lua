73cf758c721fd32b761980bd01e565e773d34bc0
ravihlb <ravihlb@protonmail.com>
2025-07-17 10:51:04 -0300

    feat(plugins): add mini.nvim modules

---
+++ lua/ravi/lazy/plugins/mini/mini.lua
@@ -0,0 +1,31 @@
+return {
+    { "echasnovski/mini.ai", version = "*", opts = {} },
+    {
+        "echasnovski/mini.files",
+        version = "*",
+        opts = {},
+        keys = {
+            {
+                "<leader>me",
+                function()
+                    local MiniFiles = require("mini.files")
+                    local _ = MiniFiles.close() or MiniFiles.open(vim.api.nvim_buf_get_name(0), false)
+                    vim.defer_fn(function()
+                        MiniFiles.reveal_cwd()
+                    end, 30)
+                end,
+            },
+            {
+                "<leader>mo",
+                function()
+                    local MiniFiles = require("mini.files")
+                    local _ = MiniFiles.close() or MiniFiles.open()
+                end,
+            },
+        },
+    },
+    { "echasnovski/mini.statusline", version = "*", opts = {} },
+    { "echasnovski/mini.sessions", version = "*", opts = {} },
+    { "echasnovski/mini.notify", version = "*", opts = {} },
+    event = "VeryLazy",
+}
