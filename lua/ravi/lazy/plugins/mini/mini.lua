fe179583f7b7e1514498418ada59bf70d1835f09
ravihlb <ravi.lacombe@theguarantors.com>
2025-07-25 12:10:00 -0300

    refactor: set `ignorecase`, `wrap` on md buffers and mini.statusline

--- lua/ravi/lazy/plugins/mini/mini.lua
+++ lua/ravi/lazy/plugins/mini/mini.lua
@@ -1,6 +1,37 @@
 return {
     { "echasnovski/mini.ai", version = "*", opts = {} },
-    { "echasnovski/mini.statusline", version = "*", opts = {} },
+    {
+        "echasnovski/mini.statusline",
+        version = "*",
+        opts = {
+            content = {
+                active = function()
+                    local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+                    local git = MiniStatusline.section_git({ trunc_width = 40 })
+                    local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+                    local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+                    local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+                    -- using math.huge (Lua's Infinity const) to force filename to truncate
+                    local filename = MiniStatusline.section_filename({ trunc_width = math.huge })
+                    local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+                    local location = MiniStatusline.section_location({ trunc_width = 75 })
+                    local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+                    return MiniStatusline.combine_groups({
+                        { hl = mode_hl, strings = { mode } },
+                        { hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
+                        "%<", -- Mark general truncate point
+                        { hl = "MiniStatuslineFilename", strings = { filename } },
+                        "%=", -- End left alignment
+                        { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+                        { hl = mode_hl, strings = { search, location } },
+                    })
+                end,
+                inactive = nil,
+            },
+            use_icons = true,
+        },
+    },
     { "echasnovski/mini.sessions", version = "*", opts = {} },
     { "echasnovski/mini.notify", version = "*", opts = {} },
     {
